1cfb2da4af95e9c92a6433bf82226a8012f3d760
n0moreCRY <EugenGavrisciuc@yandex.ru>
2022-04-03 19:22:40 +0300

    Made a setup for the possibility to start the server

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,5 @@
 export default class RestoService {
-    _apiBase = "http://localhost:3000";
+    _apiBase = "https://food-menu-react.herokuapp.com/api";
 
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`);
